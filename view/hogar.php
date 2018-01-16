fee65bf508e00d731bbf70e398a6535091dac831
jhonatan.munoz <jhonatan.munoz@phidias.com.co>
2018-01-16 11:29:40 -0500

    Carga de proyecto

---
+++ view/hogar.php
@@ -0,0 +1,27 @@
+<?php
+include_once("include.php");
+?>
+
+
+
+
+
+
+
+
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="..." alt="...">
+      <div class="caption">
+        <h3>Título de la imagen</h3>
+        <p>...</p>
+        <p>
+          <a href="#" class="btn btn-primary" role="button">Botón</a>
+          <a href="#" class="btn btn-default" role="button">Botón</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ view/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+include_once("include.php");
+include_once("index.html");
+
+?>
+
+
